ccb7e30b500f9454ec873c97a037eed8a95bfbea
João Godoi <joaogodoi.dev@gmail.com>
2024-10-01 01:55:47 -0300

    :sparkles: feat: Created ListTasks component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
+import { useState } from "react";
 import "./App.css";
 import AddTasks from "./components/AddTasks/AddTask";
 import Tasks from "./components/ListTasks/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Estudar React", isCompleted: false },
+    { id: 2, title: "Estudar Tailwind", isCompleted: true },
+    { id: 3, title: "Estudar Axios", isCompleted: false },
+  ]);
+
+  const toggleTaskCompletion = (taskId) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, isCompleted: !task.isCompleted } : task
+      )
+    );
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  };
+
   return (
-    <div>
-      <h1>Gerenciador de tarefas</h1>
-      <AddTasks />
-      <Tasks />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+        <AddTasks />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={toggleTaskCompletion}
+          onTrashClick={deleteTask}
+        />
+      </div>
     </div>
   );
 }
--- src/components/ListTasks/Tasks.jsx
+++ src/components/ListTasks/Tasks.jsx
@@ -1,5 +1,31 @@
-function Tasks() {
-  return <h1>Tasks</h1>;
+import { ChevronRightIcon, Trash2 } from "lucide-react";
+
+function Tasks({ tasks, onTaskClick, onTrashClick }) {
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => onTaskClick(task.id)}
+            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+              task.isCompleted && "line-through"
+            }`}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 text-white p-2 rounded-md">
+            <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => onTrashClick(task.id)}
+            className="bg-slate-400 text-white p-2 rounded-md"
+          >
+            <Trash2 />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks;
